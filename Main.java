d7588012f5fbd2f7661cc0d74964f4eb4ecc01c7
mutiarasywl <mutiarasyawal406@gmail.com>
2026-03-05 12:00:51 +0700

    Add files via upload
    latihan 2

--- Main.java
+++ Main.java
@@ -1,49 +1,44 @@
-class Cafe {
-    String nama;
-    String alamat;
-
-    // Constructor
-    Cafe(String nama, String alamat) {
-        this.nama = nama;
-        this.alamat = alamat;
-        System.out.println("Nama cafenya adalah : " + nama);
-        System.out.println("Alamatnya di : " + alamat);
-        System.out.println("====================");
-    }
-
-    // Method
-    void buka() {
-        System.out.println("Cafe Sudah Buka");
-    }
-
-    // Getter
-    public String getNama() {
-        return nama;
-    }
-
-    public String getAlamat() {
-        return alamat;
-    }
-
-    // Setter
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public void setAlamat(String alamat) {
-        this.alamat = alamat;
-    }
-}
+package latihan_pbo2;
 
 public class Main {
+
+    static class Cafe {
+
+        private String nama;
+        private String alamat;
+        private double panjang;
+        private double lebar;
+
+        public Cafe(String nama, String alamat, double panjang, double lebar) {
+            this.nama = nama;
+            this.alamat = alamat;
+            this.panjang = panjang;
+            this.lebar = lebar;
+        }
+
+        public double hitungLuas() {
+            return panjang * lebar;
+        }
+
+        public double hitungKeliling() {
+            return 2 * (panjang + lebar);
+        }
+
+        public void tampilkanInfo() {
+            System.out.println("Nama Cafe   : " + nama);
+            System.out.println("Alamat      : " + alamat);
+            System.out.println("Panjang     : " + panjang);
+            System.out.println("Lebar       : " + lebar);
+            System.out.println("Luas        : " + hitungLuas() + " m2");
+            System.out.println("Keliling    : " + hitungKeliling() + " m");
+            System.out.println("===========================");
+        }
+    }
+
     public static void main(String[] args) {
 
-        Cafe cafe1 = new Cafe("Kenangan", "IMKOT");
-        Cafe cafe2 = new Cafe("Janji Jiwa", "Cirebon");
-        Cafe cafe3 = new Cafe("Kopi", "Indramayu");
+        Cafe cafe1 = new Cafe("Cafe Senja", "Indramayu", 15, 10);
 
-        cafe1.buka();
-        cafe2.buka();
-        cafe3.buka();
+        cafe1.tampilkanInfo();
     }
 }
